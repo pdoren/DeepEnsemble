192b30c80860681efa03059b182eb8b959d4904d
Pablo Saavedra <pablo.saavedra@ug.uchile.cl>
2016-09-11 15:21:32 -0300

    Changed setup.py.

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='correntropy-and-ensembles-in-deep-learning',
     version='0.1',
-    packages=['libml', 'libml.utils', 'libml.utils.metrics', 'libml.layers', 'libml.models', 'libml.ensemble',
-              'libml.ensemble.combiner'],
+    packages=find_packages(),
     url='https://github.com/pdoren/correntropy-and-ensembles-in-deep-learning',
-    license='MIT License',
+    license='MIT',
     author='pdoren',
     author_email='pablo.saavedra@ug.uchile.cl',
-    description='Library for working with Ensemble Models.'
+    description='Library for working with Ensemble Models.',
+    install_requires=['numpy'],
+    extras_require={
+        'testing': [
+            'mock',
+            'pytest',
+            'pytest-cov',
+            'pytest-pep8',
+        ],
+    },
 )
