465cfa3a899d68a943c39096d842ed8045d6d23d
Pablo Saavedra <pablo.saavedra@ug.uchile.cl>
2016-09-11 20:40:36 -0300

    Test 14 ReadTheDocs.

--- libml/ensemble/combiner/weightaveragecombiner.py
+++ libml/ensemble/combiner/weightaveragecombiner.py
@@ -1,6 +1,6 @@
 import theano.tensor as T
 from .modelcombiner import ModelCombiner
-from ...utils import translate_target
+from libml.utils import translate_target
 from theano import shared
 
 __all__ = ['WeightAverageCombiner']
